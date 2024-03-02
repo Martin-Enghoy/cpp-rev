1f0d43dc158c17978edc19ed6713bad5a68dd781
Martin-Enghoy <adrianenghoy@gmail.com>
2024-03-02 15:09:13 +0800

    intro: declarations, initializations and definitions

---
+++ Intro/basic.cpp
@@ -0,0 +1,31 @@
+// Header Files
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // std::cout << "Hello, World" << std::endl;
+
+    // Case-sensitive, can contain digits, no whitespace and special characters,
+    float simpleInterest;
+    int time, speed;
+    char name;
+
+    // Constant, immutable
+    const float gravity = 9.8;
+
+    // this is declaration of variable a
+    int a;
+
+    // this is initialisation of a
+    a = 10;
+
+    // this is definition = declaration + initialisation
+    int b = 20;
+
+    cout << "Hello World!";
+
+    printf("This is the value of gravity %.2f", gravity);
+
+    return 0;
+}
